d4f483e0d011d1910899d87e82300167a5a34db2
agayushh <agayush088@gmail.com>
2025-04-06 20:03:22 +0530

    items added

---
+++ src/components/Popular.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import data_product from "../assets/all_product";
+
+export default function Popular() {
+  return (
+    <div>
+      <div>Popular in Women</div>
+      <div className="grid grid-cols-3 space-x-6 space-y-5">
+        {data_product.map((item, key) => (
+          <div className="border-2">
+            <div>
+              <div>
+                {" "}
+                <img src={item.image} alt="" />
+              </div>
+              <div>{item.id}</div>
+              <div>{item.category}</div>
+              <div>{item.name}</div>
+              <div>{item.new_price}</div>
+              <div>{item.old_price}</div>
+              <button className="bg-orange-400 flex justify-center w-full p-2 hover:bg-orange-300">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
