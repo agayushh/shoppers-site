18ddd1a25e5f71a796ea8e56dbd33093b99d40d5
agayushh <agayush088@gmail.com>
2025-07-24 18:55:52 +0530

    fix: update Navbar component to use Link for navigation and improve code structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,11 @@ function Navbar() {
     <div className="flex md:flex-row justify-between px-4 md:px-10 pt-2 md:py-4 shadow-lg">
       <div className="flex justify-center md:justify-start md:mr-16 mb-4 md:mb-0">
         <img src={shopperLogo} alt="Shopper Logo" className="h-12" />
-        <h1 className="font-normal text-2xl md:text-3xl font-mono flex justify-center items-center pt-2">
-          SHOPPER
-        </h1>
+        <Link to="/">
+          <h1 className="font-normal text-2xl md:text-3xl font-mono flex justify-center items-center pt-2">
+            SHOPPER
+          </h1>
+        </Link>
       </div>
       <div className="lg:block hidden">
         <ol className="flex flex-wrap justify-center items-center space-x-4 md:space-x-8 mt-0 md:mt-2 mr-0 md:mr-16">
@@ -46,9 +48,14 @@ function Navbar() {
         </div>
         <div className="lg:block hidden">
           <div className="flex justify-center md:justify-end gap-x-5 items-center">
-            <button className="py-2 px-4"> Login </button>
+            <Link className="py-2 px-4" to="/login">
+              {" "}
+              Login{" "}
+            </Link>
             <div className="hidden lg:block">
-              <FaShoppingCart size={24} />
+              <Link to="/cart">
+                <FaShoppingCart size={24} />
+              </Link>
             </div>
           </div>
         </div>
