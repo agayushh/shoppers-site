d4173d15d2dc92a1aeb6ab6a2638da8183c70e64
agayushh <agayush088@gmail.com>
2025-08-06 05:38:17 +0530

    feat: update database name to 'shoppper' and add ApiError class for error handling

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+  constructor(
+    statuscode,
+    message = "Something went wrong",
+    stack = "",
+    errors = []
+  ) {
+    super(message);
+    this.statuscode = statuscode;
+    this.message = message;
+    this.data = null;
+    this.errors = errors;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
