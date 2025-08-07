221418c7050c97adfab735242ad2ea954edea1bf
agayushh <agayush088@gmail.com>
2025-08-07 16:45:02 +0530

    fix: update API endpoint for user registration in signup component

--- shoppers-site/src/pages/SignUp.jsx
+++ shoppers-site/src/pages/SignUp.jsx
@@ -12,7 +12,7 @@ export default function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8000/user/api", {
+      const res = await axios.post("http://localhost:8000/api/user/register", {
         name,
         email,
         password,
