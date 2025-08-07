cb324f4346a784921dbb56d3130340424d9db826
agayushh <agayush088@gmail.com>
2025-08-07 15:34:21 +0530

    feat: implement user signup functionality with error handling and form validation

--- shoppers-site/src/pages/SignUp.jsx
+++ shoppers-site/src/pages/SignUp.jsx
@@ -1,6 +1,34 @@
-import React from "react";
-
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8000/user/api", {
+        name,
+        email,
+        password,
+      });
+      console.log(`register user: ${res.data}`);
+      if (res.data?.user?._id) {
+        navigate("/");
+      }
+      else {
+        setError(res.message || "sign up failed");
+      }
+    } catch (err) {
+      console.error("Signup error:", err);
+      setError(err.response?.data?.message || "Something went wrong.");
+    }
+  };
   return (
     <section className="min-h-screen flex items-center justify-center bg-black px-4">
       <div className="bg-zinc-900 p-8 rounded-2xl shadow-xl w-full max-w-md">
@@ -8,31 +36,46 @@ export default function Signup() {
           Sign Up
         </h2>
 
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
-            <label className="block text-gray-300 mb-1">Username</label>
+            <label htmlFor="username" className="block text-gray-300 mb-1">
+              Username
+            </label>
             <input
               type="text"
               className="w-full px-4 py-2 bg-zinc-800 border border-zinc-700 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-orange-400"
               placeholder="Your name"
+              value={name}
+              id="name"
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
 
           <div>
-            <label className="block text-gray-300 mb-1">Email</label>
+            <label htmlFor="email" className="block text-gray-300 mb-1">
+              Email
+            </label>
             <input
               type="email"
               className="w-full px-4 py-2 bg-zinc-800 border border-zinc-700 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-orange-400"
               placeholder="you@example.com"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
           <div>
-            <label className="block text-gray-300 mb-1">Password</label>
+            <label htmlFor="password" className="block text-gray-300 mb-1">
+              Password
+            </label>
             <input
               type="password"
               className="w-full px-4 py-2 bg-zinc-800 border border-zinc-700 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-orange-400"
               placeholder="Create a password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
@@ -49,6 +92,9 @@ export default function Signup() {
               Log in
             </a>
           </p>
+          {error && (
+            <p className="text-sm text-red-400 text-center mt-4">{error}</p>
+          )}
         </form>
       </div>
     </section>
